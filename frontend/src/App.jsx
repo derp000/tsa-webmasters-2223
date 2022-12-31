b4594eccf9b0b3545e8490badd031538de87cb70
Shreyas Nair <shreyasmohan14@gmail.com>
2022-12-31 16:50:38 -0500

    add about us code inside is garbage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./containers/Home";
 import TestDisclosure from "./components/TestDisclosure";
+import AboutUs from "./components/AboutUs";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/*" element={<Home />} />
       <Route path="/test" element={<TestDisclosure />} />
+      <Route path = "/aboutus" element = {<AboutUs />} />
     </Routes>
   );
 };
---
+++ frontend/src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tabs from './Tabs'
+
+const AboutUs = () => {
+  return (
+    <Tabs />
+  )
+}
+
+export default AboutUs
