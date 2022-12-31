17d29288e68c30bced4fd527887d43987a60bcce
derp000 <84947701+derp000@users.noreply.github.com>
2022-12-31 12:51:51 -0500

    Added some filler code

---
+++ frontend/README-frontend.md
@@ -0,0 +1,4 @@
+# Run app
+
+1. `cd frontend`
+1. `npm run dev`
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // [url('https://images-assets.nasa.gov/image/PIA04591/PIA04591~orig.jpg')]
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-cover bg-black">
+      <div className="bg-center bg-contain bg-no-repeat bg-[url('assets/mars.jpg')]">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-white font-bold text-6xl pb-5">Mars Interest</h1>
+          <button
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setCount(count + 1)}
+          >
+            {count} Joined!
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
