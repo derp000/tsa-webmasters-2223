1d54db46aa2cd95e59247ca886aad6f4570bcc3d
derp000 <84947701+derp000@users.noreply.github.com>
2023-02-25 23:29:31 -0500

    Changed landing page

--- frontend/src/components/HeroScreen.jsx
+++ frontend/src/components/HeroScreen.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 const HeroScreen = () => {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{ backgroundImage: `url("https://images-assets.nasa.gov/image/PIA03276/PIA03276~orig.jpg")` }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Bonjour</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Commencer</button>
+    <div>
+      <div className="absolute w-[40rem] h-20 left-32 top-80">
+        <div className="text-7xl text-white font-extrabold leading-snug">
+          Be the next astronaut.
         </div>
       </div>
+      <div className="absolute w-[40rem] h-24 left-32 top-[35rem]">
+        <div className="text-lg">
+          Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. 
+We examine annual percentage rates, annual fees.
+        </div>
+      </div>
+      <button className="absolute w-48 h-14 left-32 top-[42rem] bg-yellow-100 hover:bg-yellow-200 rounded-md"> 
+        <div className="text-2xl text-black font-bold text-center">
+          Get Started
+        </div>
+      </button>
     </div>
   );
 };
