be7660c1826a524bc6c6e75fadbb436b4794add2
derp000 <84947701+derp000@users.noreply.github.com>
2023-02-26 19:21:40 -0500

    Responsive Hero temp

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import HeroImage from "./HeroImage";
 import HeroText from "./HeroText";
 
-const cta =
-  "We're gonna get YOU to space.";
+const cta = "We're gonna get YOU to space.";
 const space_image = "https://cataas.com/cat/says/Space%20Rocks!";
+
+const heading = "The best of the best";
+const title = "Home to leading aerospace professionals.";
+const body = `
+  Helios is an international, English-speaking astronaut training 
+  program that includes vetting candidates, two-year long formal astronaut 
+  training, and a crewed Mars mission in partnership with NASA and ESA.`;
 
 const AboutUs = () => {
   return (
     <div>
       <HeroImage callToAction={cta} imgUrl={space_image} />
-      <HeroText />
+      <HeroText heading={heading} title={title} body={body} />
     </div>
   );
 };
--- frontend/src/components/HeroText.jsx
+++ frontend/src/components/HeroText.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 
-const HeroText = ({ header, title, body }) => {
+const HeroText = ({ heading, title, body, images }) => {
   return (
-    <div className="hero h-fit bg-base-200">
+    <div className="hero h-fit bg-base-200 px-5">
       <div className="hero-content text-left">
-        <div className="max-w-screen">
-          <h6 className="text-2xl font-bold mt-10">The best of the best</h6>
-          <h1 className="text-7xl font-bold mt-2">
-            Home to leading aerospace professionals.
-          </h1>
-          <div className="inline-flex gap-10 mt-5">
-            <p className="py-6 text-2xl max-w-md leading-loose">
-              Helios is an international, English-speaking astronaut training
-              program that includes vetting candidates, two-year long formal
-              astronaut training, and a crewed Mars mission in partnership with
-              NASA and ESA.
+        <div className="max-w-5xl">
+          <h6 className="md:text-2xl text-xl font-bold mt-10">{heading}</h6>
+          <h1 className="md:text-7xl text-5xl font-bold mt-2">{title}</h1>
+          <div className="md:inline-flex md:gap-10 md:mt-5">
+            <p className="py-6 lg:text-2xl text-3xl md:max-w-md lg:leading-loose leading-loose">
+              {body}
             </p>
-            <img className="mt-10 h-64 w-64" src="src/assets/NASA_logo.svg" />
-            {/* mt-10 + mt-16 */}
-            <img
-              className="h-32 w-64 mt-[6.5rem] bg-white"
-              src="src/assets/European_Space_Agency_logo.svg"
-            />
+            <div className="lg:contents md:grid md:ml-10">
+              <img
+                className="lg:mt-10 md:h-64 md:w-64 sm:h-48 sm:w-48 sm:block hidden"
+                src="src/assets/NASA_logo.svg"
+              />
+              {/* mt-10 + mt-12 or lg:mt-16  */}
+              <img
+                className="md:h-32 md:w-64 lg:mt-[6.5rem] sm:h-24 sm:w-48 sm:bg-white sm:block hidden"
+                src="src/assets/European_Space_Agency_logo.svg"
+              />
+            </div>
           </div>
         </div>
       </div>
