9193b13b0a5bc872c3917169fdb3f0a059df9f7d
derp000 <84947701+derp000@users.noreply.github.com>
2023-02-26 21:11:34 -0500

    Added location info HeroText and fixed HeroText

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -5,9 +5,7 @@ import HeroText from "./HeroText";
 const cta = "We're gonna get YOU to space.";
 const space_image = "https://cataas.com/cat/says/Space%20Rocks!";
 
-const heading = "The best of the best";
-const title = "Home to leading aerospace professionals.";
-const body = `
+const bodyIntro = `
   Helios is an international, English-speaking astronaut training 
   program that includes vetting candidates, two-year long formal astronaut 
   training, and a crewed Mars mission in partnership with NASA and ESA.`;
@@ -15,12 +13,36 @@ const nasa = (
   <img
     className="lg:mt-10 md:h-64 md:w-64 sm:h-48 sm:w-48 sm:block hidden"
     src="src/assets/NASA_logo.svg"
+    alt="Nasa Logo"
+    key="nasa_logo"
   />
 );
+// mt-10 + mt-16
 const esa = (
   <img
     className="md:h-32 md:w-64 lg:mt-[6.5rem] sm:h-24 sm:w-48 sm:bg-white sm:block hidden"
     src="src/assets/European_Space_Agency_logo.svg"
+    alt="ESA Logo"
+    key="esa_logo"
+  />
+);
+
+const bodySite = `
+  Helios is based out of Huntsville, Alabama, a world-renowned hub for aerospace. 
+  At Space Camp in Huntsville Alabama, where many youth and adults do mock astronaut training, 
+  Helios will construct their own building. Space Camp, which is internationally funded by space 
+  programs, already has many advanced features including a neutral buoyancy pool, three duplicate 
+  spacecraft for stimulations, and a newly built mars habitat stimulator. These will be shared by 
+  Space Camp and Helios for training purposes. In its own building, Helios will build a g-force 
+  accelerator, physical and mental testing rooms, and offices.
+`;
+// 3:2 image
+const huntsville = (
+  <img
+    className="mt-10 mx-auto sm:h-[48rem] h-96 w-[32rem] max-h-fit"
+    src="src/assets/HuntsvilleRocket.jpg"
+    alt="Rockets on display in Huntsville"
+    key="huntsville"
   />
 );
 
@@ -28,7 +50,19 @@ const AboutUs = () => {
   return (
     <div>
       <HeroImage callToAction={cta} imgUrl={space_image} />
-      <HeroText heading={heading} title={title} body={body} images={[nasa, esa]} />
+      <HeroText
+        heading={"The best of the best"}
+        title={"Home to leading aerospace professionals."}
+        body={bodyIntro}
+        images={[nasa, esa]}
+      />
+      <HeroText
+        heading={"Welcome to AL"}
+        title={"Born in the heart of Alabama's space industry."}
+        body={bodySite}
+        images={[huntsville]}
+        reverseAlign={true}
+      />
     </div>
   );
 };
--- frontend/src/components/HeroText.jsx
+++ frontend/src/components/HeroText.jsx
@@ -1,19 +1,48 @@
 import React from "react";
 
-const HeroText = ({ heading, title, body, images }) => {
+const HeroText = ({ heading, title, body, images, reverseAlign }) => {
+  const heroBodyText = (
+    <p
+      className="text-left py-6 lg:text-2xl text-2xl md:max-w-md lg:leading-loose leading-loose"
+      key="main_text"
+    >
+      {body}
+    </p>
+  );
+
+  let heroImages;
+  if (!images) {
+    heroImages = <></>;
+  } else {
+    heroImages =
+      images.length > 1 ? (
+        <div
+          className={`lg:contents sm:grid md:grid-cols-none sm:grid-cols-${
+            images.length
+          } place-items-center ${reverseAlign ? "md:mr-10" : "md:ml-10"}`}
+          key="images"
+        >
+          {images?.map((img) => img)}
+        </div>
+      ) : (
+        <div key="images">{images[0]}</div>
+      );
+  }
+
+  const heroBody = [heroBodyText, heroImages];
+
   return (
-    <div className="hero h-fit bg-base-200 px-5">
-      <div className="hero-content text-left">
+    <div className="hero h-fit bg-base-200 px-2 mt-5">
+      <div
+        className={`hero-content ${reverseAlign ? "text-right" : "text-left"}`}
+      >
         <div className="max-w-5xl">
           <h6 className="md:text-2xl text-xl font-bold mt-10">{heading}</h6>
           <h1 className="md:text-7xl text-5xl font-bold mt-2">{title}</h1>
-          <div className="md:inline-flex md:gap-10 md:mt-5">
-            <p className="py-6 lg:text-2xl text-3xl md:max-w-md lg:leading-loose leading-loose">
-              {body}
-            </p>
-            <div className="lg:contents sm:grid md:grid-cols-none sm:grid-cols-2 place-items-center md:ml-10">
-              {images.map((img) => img)}
-            </div>
+          <div className="md:inline-flex md:gap-10 md:mt-5 md:items-start">
+            {reverseAlign
+              ? heroBody.reverse().map((hero) => hero)
+              : heroBody.map((hero) => hero)}
           </div>
         </div>
       </div>
